d54c8f7970755cfecaabc9dbfb1a8f3a8bb442e5
MS893 <ms893@free.fr>
2025-09-11 01:53:05 +0200

    envoi github 1

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# DataScientist
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="DataScientist" />
+
+  &#xa0;
+
+  <!-- <a href="https://datascientist.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">DataScientist</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/{{YOUR_GITHUB_USERNAME}}/datascientist?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  DataScientist 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/{{YOUR_GITHUB_USERNAME}}" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Exercices Big Data et Blockchain
+
+## :sparkles: Features ##
+
+:heavy_check_mark: exercises using array of twitter handle of journalists;\
+:heavy_check_mark: exercises using hash;
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [Ruby](https://www.ruby-lang.org/fr/documentation/)
+- [Node.js](https://nodejs.org/en/)
+
+## :white_check_mark: Requirements ##
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/{{YOUR_GITHUB_USERNAME}}/datascientist
+
+# Access
+$ cd datascientist
+
+# Install dependencies
+$ yarn
+
+# Run the project
+$ yarn start
+
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/MS893" target="_blank">MS893</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ lib/00_journalists.rb
@@ -0,0 +1,125 @@
+# List of journalists on Twitter
+
+journ = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def nb_journ(handle)
+  # nb de handle
+  return handle.length
+end
+
+def plus_petit_handle(handle)
+  # plus petit handle
+  petit = handle[0]
+  handle.each do |handle|
+    if handle.length < petit.length
+      petit = handle
+    end
+  end
+  return petit
+end
+
+def plus_grand_handle(handle)
+  # plus grand handle (me servira pour la répartition)
+  grand = handle[0]
+  handle.each do |handle|
+    if handle.length > grand.length
+      grand = handle
+    end
+  end
+  return grand.length
+end
+
+def nb_journ_5char(handle)
+  # nb de handle de 5 caractères
+  nb = 0
+  handle.each do |handle|
+    if (handle.length - 1 == 5)
+      nb += 1
+    end
+  end
+  return nb
+end
+
+def nb_journ_maj(handle)
+  # nb de handle avec la première lettre en mahandleuscule
+  nb = 0
+  handle.each do |handle|
+    if handle[1] == handle[1].upcase
+      nb += 1
+    end
+  end
+  return nb
+end
+
+def tri(handle)
+  # tri par ordre alphabétique le tableau
+  return handle.sort!
+end
+
+def tri_longueur(handle)
+  # tri par ordre de taille le tableau
+  return handle.sort_by { |chaine| chaine.length }
+end
+
+def répartition(handle, greatest)
+  # répartition des handle par taille de ces derniers
+  for i in 2..greatest do
+    # car on enlève le @
+    nb = 0
+    handle.each do |nom|
+      if (nom.length == i)
+        nb += 1
+        handle.delete(nom) # supprime l'élément du tableau pour éviter de le retester
+      end
+    end
+    puts " nb de handle avec #{i-1} caractère(s) = #{nb}"
+  end
+end
+
+##### exécution #####
+
+loop do
+  puts "Fais ton choix (de 1 à 9) :"
+  puts "1. Combien y a-t-il de handle dans cette array ?"
+  puts "2. Quel est le handle le plus court de cette liste ?"
+  puts "3. Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)"
+  puts "4. Combien commencent par une majuscule (première lettre juste après le @) ?"
+  puts "5. Trie la liste de handle par ordre alphabétique."
+  puts "6. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+  puts "7. Quelle est la position dans l'array de la personne @epenser ?"
+  puts "8. Sors-moi une répartition des handle par taille de ces derniers"
+  puts "9. QUITTER"
+  print "> "
+  choix = gets.chomp
+  case choix
+  when "1"
+    puts "Longueur du tableau = #{nb_journ(journ)}"
+  when "2"
+    puts "Plus petit handle du tableau = #{plus_petit_handle(journ)}"
+  when "3"
+    puts "Nombre de handle qui contiennent 5 caractères (hors @) = #{nb_journ_5char(journ)}"
+    # autre méthode plus courte (6 car il y a le @)
+    nb_journ_5char = journ.count { |chaine| chaine.length == 6 }
+    puts "Autre possibilité = #{nb_journ_5char}"
+  when "4"
+    puts "Nombre de handle dont la 1ère lettre est une majuscule = #{nb_journ_maj(journ)}"
+  when "5"
+    # tri ordre alphabétique
+    journ_trié = tri(journ)
+    puts journ_trié
+  when "6"
+    # tri par longueur
+    journ_trié = tri_longueur(journ)
+    puts journ_trié
+  when "7"
+    # position dans l'array de la personne @epenser
+    puts "Position de @epenser #{journ.index("@epenser")}"
+  when "8"
+    # répartition par taille
+    grand = plus_grand_handle(journ)
+    répartition(journ, grand)
+  when "9", "Q", "q"
+    break
+  end
+  puts
+end
